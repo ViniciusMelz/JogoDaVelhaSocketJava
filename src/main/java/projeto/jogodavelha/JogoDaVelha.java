92a6e28bc9efe6452c22ae507ac26c5e205b1523
ViniciusMelz <viniciusgmelz@gmail.com>
2024-06-19 18:15:25 -0300

    first commit

---
+++ src/main/java/projeto/jogodavelha/JogoDaVelha.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package projeto.jogodavelha;
+
+import view.TelaJogo;
+
+/**
+ *
+ * @author vinic
+ */
+public class JogoDaVelha {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        TelaJogo telaJogo = new TelaJogo();
+        telaJogo.setVisible(true);
+    }
+    
+}
